314ffdb04100351dcd8f6a0c3d6265528608c676
henriquelima92 <henriquedelima92@hotmail.com>
2020-09-26 15:34:15 -0300

    created more wheel levels. created a mock level flow.

---
+++ Assets/Scripts/WheelAsset.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/WheelAsset", order = 1)]
+public class WheelAsset : ScriptableObject
+{
+    public Color color;
+    public Sprite sprite;
+    public AnimationCurve curve;
+}
---
+++ Assets/Scripts/WheelController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WheelController : MonoBehaviour
+{
+    [SerializeField]
+    public SpriteRenderer wheelSpriteRenderer;
+    [SerializeField] 
+    private List<WheelAsset> wheelLevels;
+    [SerializeField]
+    private int currentLevel = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartLevel(currentLevel);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.J))
+        {
+            StopLevel();
+            if (currentLevel + 1 < wheelLevels.Count)
+            {
+                currentLevel += 1;
+                StartLevel(currentLevel);
+            }
+        }
+    }
+    private void StopLevel()
+    {
+        StopAllCoroutines();
+    }
+
+    private void StartLevel(int levelIndex)
+    {
+        wheelSpriteRenderer.transform.eulerAngles = Vector3.zero;
+        wheelSpriteRenderer.color = wheelLevels[levelIndex].color;
+        wheelSpriteRenderer.sprite = wheelLevels[levelIndex].sprite;
+        StartCoroutine(Movement(levelIndex));
+    }
+    private IEnumerator Movement(int levelIndex)
+    {
+        float movementTime = 0f;
+        while (true)
+        {
+            movementTime += Time.deltaTime;
+            wheelSpriteRenderer.transform.eulerAngles = new Vector3(0f, 0f, wheelLevels[levelIndex].curve.Evaluate(movementTime));
+            yield return null;
+        }
+    }
+}
