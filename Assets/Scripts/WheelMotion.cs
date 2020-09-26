74dd9b99cb0acdb56a1afd70035b91039b78a5dd
henriquelima92 <henriquedelima92@hotmail.com>
2020-09-26 12:51:57 -0300

    created a scriptableobject to store the wheel animation curve. added a script to rotate the wheel based on the time of the animation curve.

---
+++ Assets/Scripts/WheelMotion.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/WheelMotion", order = 1)]
+public class WheelMotion : ScriptableObject
+{
+    public AnimationCurve curve;
+}
